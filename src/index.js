38d1d6328bd04922f28e89fe90eccc2e53d4e864
cleidison-soares <cleidison33@gmail.com>
2023-04-17 18:35:33 -0300

    first commit

---
+++ src/index.js
@@ -0,0 +1,15 @@
+const express = require('express')
+const app = express()
+require('dotenv').config()
+
+app.use(express.json())
+
+app.get('/', (req, res) => {
+    return res.status(200).json(`Exito no deploy, Aplicação rodando perfeitamente`)
+})
+
+const porta = process.env.PORT || 3000
+
+app.listen(porta, () => {
+    console.log(`servidor rodando na porta ${porta}`)
+})
