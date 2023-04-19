44c8022752e0f1b7d5b236c8f8ae8044592ad2f5
cleidison-soares <cleidison33@gmail.com>
2023-04-19 10:55:22 -0300

    deploy

--- src/conexao/knex.js
+++ src/conexao/knex.js
@@ -1,7 +1,7 @@
 require('dotenv').config()
 
 const knex = require('knex')({
-    client: 'pg',
+    client: 'postgresql://postgres:mf8de6B9LpHFERLwfwWd@containers-us-west-53.railway.app:6490/railway',
     connection: {
       host : 'containers-us-west-53.railway.app',
       port : 6490,
