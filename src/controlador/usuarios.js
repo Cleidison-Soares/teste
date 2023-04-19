823cbea7f59e27c3dceb84a843de1c86789406f8
cleidison-soares <cleidison33@gmail.com>
2023-04-19 11:32:20 -0300

    commit

--- src/controlador/usuarios.js
+++ src/controlador/usuarios.js
@@ -3,7 +3,7 @@ const knex = require('../conexao/knex')
 
 const listarUsuarios = async (req, res) => {
 
-    const usuarios = await knex.select().table('usuarios')
+    const usuarios = await knex.select('id', 'nome', 'email').table('usuarios')
 
     return res.status(200).json(usuarios)
 
